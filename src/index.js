de0e506637f8e7bb4f668234f6541737ecec9838
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-08-13 16:10:17 +0500

    Implement dropdown logic and formatting

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./styles.css";
 import "./modern-css-reset.css";
+
+const dropdownIcon = document.querySelector(".dropdown-top");
+dropdownIcon.addEventListener("click", () => {
+  toggleDropdown();
+});
+
+function toggleDropdown() {
+  const parentElement = document.querySelector(".dropdown");
+  const dropdownElements = parentElement.querySelectorAll(".dropdown-item");
+  dropdownElements.forEach((element) => {
+    if (!element.classList.contains("show")) {
+      element.classList.add("show");
+    } else {
+      element.classList.remove("show");
+    }
+  });
+}
